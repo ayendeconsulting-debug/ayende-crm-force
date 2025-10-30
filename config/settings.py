449c5127caa132f2e842eaa3bd715f49f6e517c0
Adesanya <adesfdevorg@gmail.com>
2025-10-30 15:24:45 -0400

    added ALLOWED_HOST Corrected

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ if CUSTOM_DOMAIN:
         f'.{CUSTOM_DOMAIN}',  # Wildcard for subdomains
     ])
 
-ALLOWED_HOSTS=.ayendecx.com,staging.ayendecx.com,ayendecx.com
+ALLOWED_HOSTS = ['.ayendecx.com', 'staging.ayendecx.com', 'ayendecx.com']
 
 
 # Application definition
