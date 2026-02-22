3612b707e15a7078e4bc193d6cbdf232485e9013
Adesanya <adesfdevorg@gmail.com>
2026-02-22 08:08:49 -0500

    Move health checks to dashboard app and add ready() method

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dashboard'
+    
+    def ready(self):
+        """Import health checks to register them with django-health-check"""
+        try:
+            from . import health_checks  # noqa: F401
+        except ImportError:
+            pass
